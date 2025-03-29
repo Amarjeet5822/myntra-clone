b5c29208909b2b34d98176d75be2e14569518252
Amarjeet Gupta <amar.bst5822@gmail.com>
2025-03-29 15:23:18 +0530

    fixed url

--- frontend/src/utils/backendApi.js
+++ frontend/src/utils/backendApi.js
@@ -1,2 +1,2 @@
-// export const api = "https://myntra-clone-h1p8.onrender.com/api"
-export const api = "http://localhost:8080/api"
+export const api = "https://myntra-clone-h1p8.onrender.com/api"
+// export const api = "http://localhost:8080/api"
